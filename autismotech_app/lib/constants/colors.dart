e9e5daa716ec4a6ce4b0af55c26eddb68c48f4fe
pavithkulathunga <pavithsky@gmail.com>
2025-04-21 12:53:29 +0530

    Create colors.dart file for global access

---
+++ autismotech_app/lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF673BB6);
+  // static const accent = Color(0xFFFFC107);
+  // static const background = Color(0xFFF5F5F5);
+  // static const text = Color(0xFF212121);
+}
