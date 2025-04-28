067f7618bf2d9f0295947efa51fd7986936ee137
pavithkulathunga <pavithsky@gmail.com>
2025-04-29 03:23:04 +0530

    Add diagnosis route

--- autismotech_app/lib/routes/app_routes.dart
+++ autismotech_app/lib/routes/app_routes.dart
@@ -1,8 +1,11 @@
+import 'package:autismotech_app/screens/diagnosis_screen.dart';
 import 'package:autismotech_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     '/': (context) => const HomeScreen(),
+    '/home': (context) => const HomeScreen(),
+    '/diagnosis': (context) => const DiagnosisScreen(),
   };
 }
