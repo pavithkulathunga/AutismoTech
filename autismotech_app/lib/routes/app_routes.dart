29bf7bd97e80cf15eb105586816ba4cd85d3a087
chey99 <101052229+it21005008@users.noreply.github.com>
2025-04-29 17:29:56 +0530

    create Emotion screen

--- autismotech_app/lib/routes/app_routes.dart
+++ autismotech_app/lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:autismotech_app/screens/diagnosis_screen.dart';
+import 'package:autismotech_app/screens/emotion_screen.dart';
 import 'package:autismotech_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,5 +8,7 @@ class AppRoutes {
     '/': (context) => const HomeScreen(),
     '/home': (context) => const HomeScreen(),
     '/diagnosis': (context) => const DiagnosisScreen(),
+    '/emotion': (context) => const EmotionScreen(),
+   
   };
 }
