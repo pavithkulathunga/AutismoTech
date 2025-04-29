de454e4d55ce605872df64e64f901efdc86ca1e8
chey99 <101052229+it21005008@users.noreply.github.com>
2025-04-30 02:16:00 +0530

    created music screen

---
+++ autismotech_app/lib/screens/music_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MusicScreen extends StatefulWidget {
+  const MusicScreen({super.key});
+
+  @override
+  State<MusicScreen> createState() => _MusicScreenState();
+}
+
+class _MusicScreenState extends State<MusicScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
