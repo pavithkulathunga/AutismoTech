a601dd3ab176b1bca6cc702fa71a50f80997f56b
chey99 <101052229+it21005008@users.noreply.github.com>
2025-05-06 05:07:23 +0530

    added UI featuers in emotion theropy screen

--- autismotech_app/lib/screens/emotion_screen.dart
+++ autismotech_app/lib/screens/emotion_screen.dart
@@ -21,62 +21,78 @@ class _EmotionScreenState extends State<EmotionScreen> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 10.0, 16.0, 260.0),
-        child: Align(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.green[900],
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16.0,
-                    horizontal: 24.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  elevation: 6,
-                ),
-                icon: const Icon(Icons.videogame_asset),
-                label: const Text('Play Games', style: TextStyle(fontSize: 18)),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/music'); /////link to games
-
-                  // Navigate to games screen
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.green[900],
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16.0,
-                    horizontal: 24.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  elevation: 6,
-                ),
-                icon: const Icon(Icons.music_note),
-                label: const Text(
-                  'Music Therapy',
-                  style: TextStyle(fontSize: 18),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/music');
-                  // Navigate to music therapy screen
-                },
-              ),
-            ],
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 50,
+            child: Container(
+              width: 150,
+              height: 150,
+              child: Image.asset('assets/images/eappface.png'),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 10.0, 16.0, 260.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.green[900],
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 16.0,
+                        horizontal: 24.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      elevation: 6,
+                    ),
+                    icon: const Icon(Icons.videogame_asset),
+                    label: const Text(
+                      'Play Games',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/music'); /////link to games
+
+                      // Navigate to games screen
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.green[900],
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 16.0,
+                        horizontal: 24.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      elevation: 6,
+                    ),
+                    icon: const Icon(Icons.music_note),
+                    label: const Text(
+                      'Music Therapy',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/music');
+                      // Navigate to music therapy screen
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
