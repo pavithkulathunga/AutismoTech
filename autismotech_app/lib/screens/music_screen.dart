0935d4f200c310298e6744d417cccc81f54f818f
chey99 <101052229+it21005008@users.noreply.github.com>
2025-05-14 00:10:09 +0530

    add 12 music and update colors of playlist

--- autismotech_app/lib/screens/music_screen.dart
+++ autismotech_app/lib/screens/music_screen.dart
@@ -23,6 +23,31 @@ class _MusicScreenState extends State<MusicScreen> {
 
   int? currentlyPlayingIndex; // Track index of currently playing song
   bool isPlaying = false;
+  Duration currentPosition = Duration.zero;
+  Duration totalDuration = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _audioPlayer.onDurationChanged.listen((duration) {
+      setState(() {
+        totalDuration = duration;
+      });
+    });
+
+    _audioPlayer.onPositionChanged.listen((position) {
+      setState(() {
+        currentPosition = position;
+      });
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        isPlaying = false;
+      });
+    });
+  }
 
   Future<void> _playLocalTrack(String filePath, int index) async {
     final fileName = filePath.split('/').last;
@@ -50,6 +75,7 @@ class _MusicScreenState extends State<MusicScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: const Text(
           "Music Therapy for Kids",
@@ -67,58 +93,127 @@ class _MusicScreenState extends State<MusicScreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Autism Music Therapy Playlist",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 0, 255, 55),
+              Color.fromARGB(255, 0, 92, 26),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Autism Music Therapy Playlist",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: playlist.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 16,
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: playlist.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color:
+                          [
+                            Color.fromARGB(255, 5, 209, 255),
+                            Color.fromARGB(255, 255, 242, 0),
+                            Color.fromARGB(255, 241, 0, 165),
+                          ][index % 3],
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage(playlist[index]['image']!),
-                      ),
-                      title: Text(playlist[index]['song']!),
-                      subtitle: Text(playlist[index]['artist']!),
-                      trailing: IconButton(
-                        icon: Icon(
-                          currentlyPlayingIndex == index && isPlaying
-                              ? Icons.pause
-                              : Icons.play_arrow,
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 16,
                         ),
-                        onPressed: () {
-                          _playLocalTrack(playlist[index]['file']!, index);
-                        },
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            playlist[index]['image']!,
+                          ),
+                        ),
+                        title: Text(playlist[index]['song']!),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(playlist[index]['artist']!),
+                            Text(
+                              currentlyPlayingIndex == index
+                                  ? "${_formatDuration(currentPosition)} / ${_formatDuration(totalDuration)}"
+                                  : "",
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.skip_previous),
+                              onPressed:
+                                  index > 0
+                                      ? () => _playLocalTrack(
+                                        playlist[index - 1]['file']!,
+                                        index - 1,
+                                      )
+                                      : null,
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                currentlyPlayingIndex == index && isPlaying
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                              ),
+                              onPressed: () {
+                                _playLocalTrack(
+                                  playlist[index]['file']!,
+                                  index,
+                                );
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.skip_next),
+                              onPressed:
+                                  index < playlist.length - 1
+                                      ? () => _playLocalTrack(
+                                        playlist[index + 1]['file']!,
+                                        index + 1,
+                                      )
+                                      : null,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$minutes:$seconds";
   }
 }
